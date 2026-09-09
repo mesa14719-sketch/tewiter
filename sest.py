43eff02a3da6bbc65a320a20804ca3e4ef5463e6
المطور إبراهيم | • 𝑷𝒚𝒕𝒉𝒐𝒏 • <mesa14719@gmail.com>
2026-09-09 08:37:08 +0100

    Update sest.py

--- sest.py
+++ sest.py
@@ -6,7 +6,7 @@ from user_agent import *
 
 OK = 0
 BAD = 0
-Lock = threading.Lock()
+
 
 
 R = '\x1b[38;5;1m'   # أحمر
@@ -14,10 +14,10 @@ M = '\x1b[38;5;244m' # رمادي
 L = '\x1b[38;5;10m' #اخضر 
 
 
-tok =input(f"{L} TOKEN :{R}")
+tok =input(f"{L} [{R} TOKEN {L}] :")
 os.system("clear")
 
-id =input(f"{L} ID :{R}")
+id =input(f"{L} [{R} ID {L}] :")
 os.system("clear")
 
 if not tok or not id:
@@ -33,58 +33,30 @@ def send_telegram(message):
     except:
         pass
 
-print(
-	f"{L}{'—'*60}\n"
-	f"{L} [1] {M} yopmail \n"
-	f"{L}{'—'*60}\n"
-	f"{L} [2] {M} hi2.in \n"
-	f"{L}{'—'*60}\n"
-	f"{L} [3] {M} telegmail \n"
-	f"{L}{'—'*60} \n"
-	)       
-        
-choice =input(f"{L} choice :").strip()
-os.system("clear")
-
-
-
-if not choice:
-	print(R+"   لازم تحتار دومين منهم  ")
-	sys.exit()
-
-elif choice == "1":
-	domain = "@yopmail.com"
-elif choice == "2":
-	domain = "@hi2.in"
-elif choice == "3":
-	domain = "@telegmail.com"
-
+     
 ibra = "qwertyuioplkjhgfdsamnbvcxz"
+
 while True:
 	len = random.randint(3,5)
-	email ="".join(random.choice(ibra) for _ in range (len))  + domain 
+	email ="".join(random.choice(ibra) for _ in range (len))  + "@yopmail.com"
 	
 	headers = {
 	        'accept': '*/*',
 	        'accept-language': 'ar',
 	        'origin': 'https://x.com',
 	        'referer': 'https://x.com/',
-	        'user-agent': generate_user_agent()
+	        'user-agent': str(generate_user_agent())
 	    }
 	    
 	params = {'email': email}
 	    
 	response = requests.get('https://api.x.com/i/users/email_available.json', params=params, headers=headers).text
-	  
-	    
-	   
-	   
 	   
 	if '"taken":true' in response:
 		
 		b1 = f" NEW hits Twiter ✅"
 		b2 = f" email : {email}"
-		b3 = f" Developer : @BR_Go_16 "
+		b3 = f" Developer : @I_Z_E_E "
 		msg = f"{b1}\n{b2}\n{b3}"
 		send_telegram(msg)
 		
